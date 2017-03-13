a0e63eb2cb16656af5a1b56bdcd790111e047f48
logan11116 <logan11116@mail.ru>
2017-03-14 00:04:03 +0200

    mod. F2

--- lab2/lab2/Program.cs
+++ lab2/lab2/Program.cs
@@ -20,15 +20,16 @@ namespace lab2
         {
             while(true)
             {
-                Console.WriteLine(new string(' ', 20) + "Ce faci");
+                ThreadStart F12 = new ThreadStart(F1);
+            Thread thread = new Thread(F1);
+            thread.Start();
+                Thread.Sleep(1000);
+
             }
         }
         static void Main()
         {
-            ThreadStart F12 = new ThreadStart(F1);
-            Thread thread = new Thread(F1);
-            thread.Start();
-
+            
             ThreadStart F13 = new ThreadStart(F2);
             Thread thread1 = new Thread(F2);
             thread1.Start();
