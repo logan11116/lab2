7c222e5cd6e94ba85ed148aa51e9f7311d54ebd5
logan11116 <logan11116@mail.ru>
2017-03-13 21:12:58 +0200

    Start 2 thread

--- lab2/lab2/Program.cs
+++ lab2/lab2/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace lab2
 {
@@ -10,10 +11,23 @@ namespace lab2
     {
         static void F1()
         {
-
+            while (true)
+            {
+                Console.WriteLine(new string(' ',10)+"Salut");
+            }
         }
-        static void Main(string[] args)
+        static void Main()
         {
+            ThreadStart F12 = new ThreadStart(F1);
+            Thread thread = new Thread(F1);
+            thread.Start();
+
+            while(true)
+            {
+                Console.WriteLine("Roman");
+            }
+
+            Console.ReadKey();
         }
     }
 }
