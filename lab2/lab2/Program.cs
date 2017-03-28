734186a6303a2e481aa870cac370a8ec55f08cb1
logan11116 <logan11116@mail.ru>
2017-03-28 08:38:39 +0300

    Raport + code

--- lab2/lab2/Program.cs
+++ lab2/lab2/Program.cs
@@ -1,46 +1,103 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Threading;
+using System.Threading.Tasks;
+using System.IO;
+using System.Data;
 
-namespace lab2
+
+public class Example
 {
-    class Program
+    // A semaphore that simulates a limited resource pool.
+    //
+    private static Semaphore _pool;
+
+    // A padding interval to make the output more orderly.
+    private static int _padding;
+
+
+    static CountdownEvent _countdown = new CountdownEvent(1);
+    static CountdownEvent _countdown2 = new CountdownEvent(1);
+    static CountdownEvent _countdown3 = new CountdownEvent(1);
+
+
+    public static void Main()
     {
-        static void F1()
-        {
-            while (true)
-            {
-                Console.WriteLine(new string(' ',10)+"Salut");
-            }
-        }
-         static void F2()
-        {
-            while(true)
-            {
-                ThreadStart F12 = new ThreadStart(F1);
-            Thread thread = new Thread(F1);
-            thread.Start();
-                Thread.Sleep(1000);
 
-            }
-        }
-        static void Main()
-        {
-            
-            ThreadStart F13 = new ThreadStart(F2);
-            Thread thread1 = new Thread(F2);
-            thread1.Start();
 
-            while (true)
-            {
-                Console.WriteLine("Roman");
-            }
 
-            Console.ReadKey();
-            thread1.IsBackground = true;
-        }
+        Example F1 = new Example();
+        Example F2 = new Example();
+        Example F3 = new Example();
+        Example F4 = new Example();
+        Example F5 = new Example();
+     
+
+        Thread tid1 = new Thread(new ThreadStart(F1.T1));
+        Thread tid2 = new Thread(new ThreadStart(F2.T2));
+        Thread tid3 = new Thread(new ThreadStart(F3.T3));
+        Thread tid4 = new Thread(new ThreadStart(F4.T4));
+        Thread tid5 = new Thread(new ThreadStart(F5.T5));
+
+        tid1.Name = "Thread 1";
+        tid2.Name = "Thread 2";
+        tid3.Name = "Thread 3";
+        tid4.Name = "Thread 4";
+        tid5.Name = "Thread 5";
+       
+
+        tid1.Start();
+        tid2.Start();
+        tid3.Start();
+        tid4.Start();
+        tid5.Start();
+
+
+        Console.ReadKey();
+
+
     }
+
+    public void T1()
+    {
+        Thread F = Thread.CurrentThread;
+        Console.WriteLine(F.Name);
+        _countdown.Signal();
+
+    }
+
+    public void T2()
+    {
+        _countdown.Wait();
+        Thread F = Thread.CurrentThread;
+        Console.WriteLine(F.Name);
+        _countdown.Signal();
+        _countdown2.Signal();
+
+    }
+
+    public void T3()
+        {
+        _countdown.Wait();
+        Thread F = Thread.CurrentThread;
+        Console.WriteLine(F.Name);
+        _countdown3.Signal();
+    }
+
+    public void T4()
+    {
+        _countdown2.Wait();
+        Thread F = Thread.CurrentThread;
+        Console.WriteLine(F.Name);
+       
+    }
+
+    public void T5()
+    {
+        _countdown2.Wait();
+        Thread F = Thread.CurrentThread;
+        Console.WriteLine(F.Name);
+        
+    }
+
+    
 }
---
+++ lab2/lab2sub/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace TestSemaphore
+{
+    public class Semaphore
+    {
+        private Mutex[] m_mutex;
+        private Thread[] m_threadIds;
+        private int m_threadLimit;
+
+        // constructor
+        // parameter - threadLimit (number of threads to control)
+        public Semaphore(int threadLimit)
+        {
+            m_threadLimit = threadLimit;
+            m_mutex = new Mutex[m_threadLimit];
+            m_threadIds = new Thread[m_threadLimit];
+            for (int i = 0; i < m_threadLimit; i++)
+            {
+                m_mutex[i] = new Mutex();
+                m_threadIds[i] = null;
+            }
+        }
+
+        // if there is a timeout then WaitHandle.WaitTimeout is returned
+        // calling thread should check for this
+        public int Wait()
+        {
+            int index = WaitHandle.WaitAny(m_mutex);
+            if (index != WaitHandle.WaitTimeout)
+                m_threadIds[index] = Thread.CurrentThread;
+            return index;
+        }
+
+        // release the mutex locked by the thread
+        public void Release()
+        {
+            for (int i = 0; i < m_threadLimit; i++)
+            {
+                if (m_threadIds[i] == Thread.CurrentThread)
+                {
+                    m_mutex[i].ReleaseMutex();
+                }
+            }
+        }
+
+    }
+} 
